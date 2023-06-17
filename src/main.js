714916b6491e6b42c37aa10bf451c9c8a7e97ad4
Kamilucha <brnkamila2@gmail.com>
2023-06-17 16:09:03 +0300

    Fix(js)
    import file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import * as allCategoriesList from './js/home/allCategoriesList'
+// import * as allCategoriesList from './js/home/allCategoriesList'
 import './js/scrollup'
+import './js/home/allCategoriesList'
 
 
 // import {BooksAPIService} from './js/booksAPIService'
