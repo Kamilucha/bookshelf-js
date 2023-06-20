56c6ea1bda3ea1b35b33fd40f640169f8fdb8d33
Olga16119 <tkachenko93@ukr.net>
2023-06-20 21:49:34 +0200

    add background

--- src/js/header.js
+++ src/js/header.js
@@ -1,36 +1,17 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    // const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+  
+    const toggleMenu = () => {
+      mobileMenu.classList.toggle('is-open');
+      openMenuBtn.classList.toggle(`.active_btn`)
+//  
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
 
 
-// отримаємо посилання з навігаціі і додаємо обробника подій
-const navigationLink = document.querySelectorAll('.nav-item');
-
-navigationLink.forEach(link => link.addEventListener('click', onNavLinkClick));
-
-function onNavLinkClick() {
-  navigationLink.classList.add('active');
-}
 
