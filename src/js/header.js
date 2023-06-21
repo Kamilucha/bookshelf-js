ed2c10f99516f92a4180c06fa24a08b14ef1c7de
Olga16119 <tkachenko93@ukr.net>
2023-06-21 13:58:38 +0200

    add close button

--- src/js/header.js
+++ src/js/header.js
@@ -1,34 +1,23 @@
+// отримаємо посилання на кнопку меню і додаємо слухача
+const mobileMenu = document.querySelector('.menu-container');
+const btmMenu = document.querySelector('.button-menu')
+
+btmMenu.addEventListener('click', onBtnMenuClick)
+
+function onBtnMenuClick() {
+  btmMenu.classList.toggle('active')
+  mobileMenu.classList.toggle('is-open')
+}
 
 
-(() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-            openMenuBtn.classList.toggle(`.active_btn`)
+// отримаємо посилання на навігацію і додаємо слухача
+const activePage = window.location.pathname;
+const navLink = document.querySelectorAll('.nav-item')
+  .forEach(link => {
 
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
-
-
+    if (link.href === activePage) {
+      link.setAttribute('aria-current','page')
+    }
+}
+)
+// 
