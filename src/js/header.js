262d8397d18332e7e7d7719632c660b311fe18ab
Oleksandra444 <oleksandra.work.4.4.4@gmail.com>
2023-06-22 16:16:25 +0400

    buttons-header-fix tablet

--- src/js/header.js
+++ src/js/header.js
@@ -9,40 +9,49 @@ function onBtnMenuClick() {
   mobileMenu.classList.toggle('is-open')
 }
 
+// -------Change button current page
 
-// отримаємо посилання на навігацію і додаємо слухача
-// const activePage = window.location.pathname;
-// const navLink = document.querySelectorAll('.nav-item')
-//   .forEach(link => {
-
-//     if (link.href === activePage) {
-//       link.setAttribute('aria-current','page')
-//     }
-// })
-
-
-// 
 document.addEventListener("DOMContentLoaded", function() {
-  const activePage = window.location.pathname;
-  const navLinks = document.querySelectorAll('.nav-item');
-
-  navLinks.forEach(link => {
-    if (link.href === activePage) {
-      link.setAttribute('aria-current', 'page');
-    }
-  });
-
-  const path = window.location.pathname;
+  let path = window.location.pathname;
+  console.log(path);
+  if (path === '/') {
+    path = '/index.html';
+  }
+  console.log(path);
   const navItems = document.querySelectorAll('.nav-item-li');
 
   navItems.forEach(function(item) {
     const link = item.querySelector('a');
     const href = link.getAttribute('href');
+    
 
-    if (href === path) {
+    if (path === href) {
       item.classList.add('active');
     } else {
       item.classList.remove('active');
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let path = window.location.pathname;
+  console.log(path);
+  if (path === '/') {
+    path = '/index.html';
+  }
+  console.log(path);
+  const navItems = document.querySelectorAll('.nav-item-mobile');
+
+  navItems.forEach(function(item) {
+    const link = item.querySelector('a');
+    const href = link.getAttribute('href');
+    
+
+    if (path === href) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+});
+
