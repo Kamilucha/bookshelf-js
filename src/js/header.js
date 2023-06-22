412c126eae02047fb4c08a7818c574ef5006c5b9
Oleksandra444 <oleksandra.work.4.4.4@gmail.com>
2023-06-22 12:37:16 +0400

    header current page-light button

--- src/js/header.js
+++ src/js/header.js
@@ -19,37 +19,30 @@ function onBtnMenuClick() {
 //       link.setAttribute('aria-current','page')
 //     }
 // })
-const activePage = window.location.pathname; 
-const navLink = document.querySelectorAll('.nav-item') 
-  .forEach(link => { 
- 
-    if (link.href === activePage) { 
-      link.setAttribute('aria-current','page') 
-    } 
-} 
-) 
-//  
-const path = window.location.pathname; 
-console.log(path); 
-const navItems = document.querySelectorAll('.nav-item-li'); 
- 
-   
-navItems.forEach(function (item) { 
-     
-  const link = item.querySelector('a'); 
- 
- 
-     
-  const href = link.getAttribute('href'); 
-  console.log(href); 
- 
-     
-  if (href === path) { 
-       
-    item.classList.add('active'); 
-       
-  } 
-  else { 
-    item.classList.remove('active'); 
-  } 
+
+
+// 
+document.addEventListener("DOMContentLoaded", function() {
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll('.nav-item');
+
+  navLinks.forEach(link => {
+    if (link.href === activePage) {
+      link.setAttribute('aria-current', 'page');
+    }
+  });
+
+  const path = window.location.pathname;
+  const navItems = document.querySelectorAll('.nav-item-li');
+
+  navItems.forEach(function(item) {
+    const link = item.querySelector('a');
+    const href = link.getAttribute('href');
+
+    if (href === path) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
 });
